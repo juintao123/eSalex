326e7f1744ae93a39ce329c7fe42ae8a3a4af039
juintao123 <1102137149@gm.kuas.edu.tw>
2017-03-17 15:21:00 +0800

    1

---
+++ eSalex/Models/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSalex.Models
+{
+
+    public class OrderService
+    {
+
+        public void InsertOrder()
+        {
+
+        }
+
+        public void DeleteOrderById()
+        {
+
+        }
+
+        public void UpdateOrder()
+        {
+
+        }
+
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "123";
+            result.CustName = "TAO";
+            return result;
+        } 
+
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+    }
+}
